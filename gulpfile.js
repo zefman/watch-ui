536672c770b9dfc8eed046183dea86bd69d757ca
Jozef Maxted <jozef.maxted@theunit.co.uk>
2015-08-06 15:50:34 +0100

    Compile bower components to vendor.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
-var gulp = require( 'gulp' );
-var sass = require( 'gulp-sass' );
+var gulp   = require( 'gulp' );
+var sass   = require( 'gulp-sass' );
+var concat = require( 'gulp-concat' );
 
 gulp.task( 'sass', function () {
-  gulp.src( './sass/**/*.scss' )
-    .pipe( sass().on( 'error', sass.logError ) )
-    .pipe( gulp.dest( './css' ) );
+    gulp.src( './sass/**/*.scss' )
+        .pipe( sass().on( 'error', sass.logError ) )
+        .pipe( gulp.dest( './css' ) );
 } );
+
+gulp.task( 'scripts', function() {
+    return gulp.src( './bower_components/jquery/dist/jquery.min.js' )
+        .pipe( concat( 'vendor.js' ) )
+        .pipe( gulp.dest( './js/' ) );
+});
